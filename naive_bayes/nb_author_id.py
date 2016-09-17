98fd66cadaa700784e15d845d4e1d69bcfd575d4
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-16 20:10:05 -0500

    added timing analysis to naive bayes exercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -28,9 +28,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
 pred = clf.predict(features_test)
-print(clf.score(features_test, labels_test))
+print "predicting time:", round(time()-t1, 3), "s"
+t2 = time()
+print "accuracy: ", clf.score(features_test, labels_test)
+print "scoring time:", round(time()-t2, 3), "s"
 
 #########################################################
 
