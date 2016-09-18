7619cea3412fbc9d6ee007acdda422ffa57f9dbe
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-18 11:50:13 -0500

    implemented Decision Trees mini-project

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+print len(features_train[0])
 
 #########################################################
 
