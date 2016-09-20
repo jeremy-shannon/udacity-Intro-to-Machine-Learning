d3bdb08148ecef05e186cec125108696c8fbe469
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-19 21:26:34 -0500

    outliers mini-project - slope w outliers

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,7 +26,10 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
+from sklearn.linear_model import  LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print reg.coef_[0]
 
 
 
