e3938210853f428c2cb87621d89d43dca85369a5
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-19 20:02:23 -0500

    regression mini-project - slope and intercept

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +38,12 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train,target_train)
 
+print "slope:", reg.coef_[0]
+print "intercept:",reg.intercept_
 
 
 
