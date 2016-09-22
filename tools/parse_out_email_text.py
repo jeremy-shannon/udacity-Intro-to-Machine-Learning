74f146ed6182d5dbd1935af63a8cadc8b775d785
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-21 19:56:40 -0500

    Text Learning mini-project - deploying stemming

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,21 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
 
+        wordsIn = string.split(text_string, ' ')
+        words = ""
 
-
+        from nltk.stem.snowball import SnowballStemmer
+        stemmer = SnowballStemmer("english")
+        for word in wordsIn:
+            if word:
+                words += stemmer.stem(word)
+                words += " "
 
     return words
 
