c6782d8efce7a096a113a707b4a9cc18e49454d2
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-09-23 21:59:17 -0500

    Evaluation mini-project complete

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -29,3 +29,20 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train , labels_test = train_test_split(features,\
+    labels, random_state=42, test_size=0.3)
+
+### it's all yours from here forward!  
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "accuracy:", clf.score(features_test, labels_test)
+predictions = clf.predict(features_test)
+print "predictions:", predictions
+print "actuals:", labels_test
+
+from sklearn import metrics
+print "precision:", metrics.precision_score(labels_test, predictions)
+print "recall:", metrics.recall_score(labels_test, predictions)
